e3a6b4b72333cce5b230e952ff345ac615e5224b
TotnhatTorob <zahar_sochkov@mail.ru>
2025-04-05 18:35:47 +0300

    final commit

--- main.py
+++ main.py
@@ -22,7 +22,8 @@ def startBot(message):
     bot.send_photo(chat_id=message.chat.id, 
         photo=open(os.path.join('media', 'start.JPEG'), 'rb'),
         caption=start_text,
-        reply_markup=markup
+        reply_markup=markup,
+        parse_mode="Markdown"
         )
     #bot.send_message(message.chat.id, start_text, reply_markup=markup)
 
@@ -40,37 +41,39 @@ def forward_check(message):
     markup = types.InlineKeyboardMarkup()
     btn = types.InlineKeyboardButton(text="Написать менеджеру", callback_data='btn_8')
     markup.add(btn)
-    bot.send_message(message.chat.id, text_7, reply_markup=markup)
+    bot.send_message(message.chat.id, text_7, reply_markup=markup, parse_mode='Markdown')
 
 @bot.callback_query_handler(func=lambda call: True)
 def callback_r(call):
     if call.data == "btn_1":
         markup = types.InlineKeyboardMarkup()
-
-        bot.send_media_group(call.message.chat.id, [
-            telebot.types.InputMediaPhoto(open(os.path.join('media', 'second.JPG'), 'rb')),
-            telebot.types.InputMediaPhoto(open(os.path.join('media', 'second_2.JPG'), 'rb'))
-            ]
-            )
-
         btn = types.InlineKeyboardButton(text="Программа NOVAя INTENSIVE", callback_data='btn_2')
         markup.add(btn)
-        bot.send_message(call.message.chat.id, text_1, reply_markup=markup)
+        bot.send_photo(chat_id=call.message.chat.id,
+                       photo=open('media/second_2.JPG', 'rb'),
+                       caption=text_1,
+                       reply_markup=markup,
+                       parse_mode="Markdown"
+                       )
 
     elif call.data == "btn_2":
         markup = types.InlineKeyboardMarkup()
         btn = types.InlineKeyboardButton(text="Что нужно для тренировок?", callback_data='btn_3')
         markup.add(btn)
-        bot.send_message(call.message.chat.id, text_2, reply_markup=markup)
+        bot.send_message(call.message.chat.id, text_2, reply_markup=markup, parse_mode="Markdown")
 
     elif call.data == "btn_3":
         markup = types.InlineKeyboardMarkup()
         btn = types.InlineKeyboardButton(text="Хочу попасть на интенсив", callback_data='btn_4')
         markup.add(btn)
-        bot.send_message(call.message.chat.id, text_3, reply_markup=markup)
+        bot.send_photo(chat_id=call.message.chat.id,
+                       photo=open('media/second.JPG', 'rb'),
+                       caption=text_3,
+                       reply_markup=markup,
+                       parse_mode="Markdown"
+                       )
 
     elif call.data == "btn_4":
-        bot.delete_message(call.message.chat.id, call.message.id)
         bot.send_media_group(call.message.chat.id, [
             telebot.types.InputMediaPhoto(open(os.path.join('media', 'review_1.jpg'), 'rb')),
             telebot.types.InputMediaPhoto(open(os.path.join('media', 'review_2.PNG'), 'rb')),
@@ -83,26 +86,26 @@ def callback_r(call):
         markup = types.InlineKeyboardMarkup()
         btn = types.InlineKeyboardButton(text="Зарегистрироваться на интенсив", callback_data='btn_5')
         markup.add(btn)
-        bot.send_message(call.message.chat.id, text_4, reply_markup=markup)
+        bot.send_message(call.message.chat.id, text_4, reply_markup=markup, parse_mode="Markdown")
 
     elif call.data == "btn_5":
         markup = types.InlineKeyboardMarkup()
         btn = types.InlineKeyboardButton(text="Да, готова", callback_data='btn_6')
         markup.add(btn)
-        bot.send_message(call.message.chat.id, text_5, reply_markup=markup)
+        bot.send_message(call.message.chat.id, text_5, reply_markup=markup, parse_mode="Markdown")
 
     elif call.data == "btn_6":
-        msg = bot.send_message(call.message.chat.id, text_6)
+        msg = bot.send_message(call.message.chat.id, text_6, parse_mode="Markdown")
         bot.register_next_step_handler(msg, forward_check)        
 
     elif call.data == "btn_8":
         markup = types.InlineKeyboardMarkup()
         btn = types.InlineKeyboardButton(text="Вернуться к оплате", callback_data='btn_6')
         markup.add(btn)
-        bot.send_message(call.message.chat.id, f'Наш менеджер {manager} поможет вам и ответит на ваши вопросы', reply_markup=markup)
+        bot.send_message(call.message.chat.id, f'Наш менеджер {manager} поможет вам и ответит на ваши вопросы', reply_markup=markup, parse_mode="Markdown")
 
     elif call.data == "btn_link":
-        bot.send_message(call.message.chat.id, f'Вот ссылка на наш чат {link_guest}\nЖдем тебя!')
+        bot.send_message(call.message.chat.id, f'Вот ссылка на наш чат {link_guest}\nЖдем тебя!', parse_mode="Markdown")
 
 
 
