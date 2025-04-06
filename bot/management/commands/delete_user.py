145a7bdde159d19b015c16540bd0d34d1a629542
printnewcode <zenit200803@mail.ru>
2025-04-06 17:05:55 +0300

    fix

--- bot/management/commands/delete_user.py
+++ bot/management/commands/delete_user.py
@@ -14,7 +14,7 @@ class Command(BaseCommand):
     def handle(self, *args, **options):
         users = User.objects.all()
         for user in users:
-            is_active = access_time.is_active(user)
+            is_active = is_active(user)
             if not is_active:
                 bot.ban_chat_member(chat_id=CHAT_ID, user_id=int(user.telegram_id), revoke_messages=False)
                 bot.send_message(
